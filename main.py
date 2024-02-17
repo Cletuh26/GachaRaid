7abd5c5a9d83e78c76d987f6d91271004d7e94eb
Cletuh26 <ismaelx26@gmail.com>
2024-02-17 14:52:10 +0100

    primer commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# from controller import PersonajesController
+
+# def main():
+#     controlador = PersonajesController()
+#     controlador.get_user_details()
+#     controlador.show_user_details()
+
+# if __name__ == "__main__":
+#     main()
+
+
+from controller import UserController
+
+def main():
+    controller = UserController()
+    controller.get_user_details()
+    controller.show_user_details()
+
+if __name__ == "__main__":
+    main()
