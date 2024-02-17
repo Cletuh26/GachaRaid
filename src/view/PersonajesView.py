7ad3e7a9d8223b928f2af9a39ad195c801674cc3
Cletuh26 <ismaelx26@gmail.com>
2024-02-17 15:25:22 +0100

    Creado UserModel

---
+++ src/view/PersonajesView.py
@@ -0,0 +1,7 @@
+# class PersonajesView:
+#     def show_user_details(self, personaje):
+#         print(f"Nombre: {personaje.nombre}, Email: {personaje.email}")
+
+class UserView:
+    def show_user_details(self, user):
+        print(f"Name: {user.name}, Email: {user.email}")
---
+++ src/view/UserView.py
@@ -0,0 +1,8 @@
+# class PersonajesView:
+#     def show_user_details(self, personaje):
+#         print(f"Nombre: {personaje.nombre}, Email: {personaje.email}")
+
+class UserView:
+
+    def show_user_details(self, user):
+        print(f"Name: {user.name}, Email: {user.email}")
